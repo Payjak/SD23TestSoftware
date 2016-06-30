4ccb7f67a64a301ff267f6efced1468a95d61dfe
joshuajanolo <joshua_adam.janolo@robertsoncollege.net>
2016-06-30 16:59:40 -0500

    Login in Progress

---
+++ SDQuizMaker/SDQuizMaker/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using SDQuizMaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SDQuizMaker.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login obj)
+        {
+            Model1 db = new Model1();
+            var us = db.tbusers.FirstOrDefault(m => (m.Email == obj.Email) && m.Password == obj.Password);
+            if (us == null)
+            {
+                TempData["msg"] = "<script>alert('Wrong email or password');</script>";
+            }
+            else if (us.AccessLevel == "student")
+            {
+                Session["Email"] = us.Email;
+                Session["AccessLevel"] = us.AccessLevel;
+                Session["UserID"] = us.UserID;
+                return RedirectToAction("Index", "Home");
+            }
+            else if (us.AccessLevel == "mentor")
+            {
+                Session["Email"] = us.Email;
+                Session["AccessLevel"] = us.AccessLevel;
+                Session["UserID"] = us.UserID;
+                return RedirectToAction("Index", "Home");
+            }
+            else if (us.AccessLevel == "admin")
+            {
+                Session["Email"] = us.Email;
+                Session["AccessLevel"] = us.AccessLevel;
+                Session["UserID"] = us.UserID;
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ SDQuizMaker/SDQuizMaker/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace SDQuizMaker.Models
+{
+    [Table("tbuser")]
+    public class Login
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AccessLevel { get; set; }
+    }
+}
